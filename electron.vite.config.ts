97e630dd909b18d9bc96ffe417a9f4f1c600862e
hello8693 <hello8693@hello8693.xyz>
2025-08-21 22:36:16 +0800

    fix Remove Tailwind CSS configuration and related files; update component declarations in TypeScript definitions to reflect new component structure; clean up main CSS file by removing Tailwind directives.

--- electron.vite.config.ts
+++ electron.vite.config.ts
@@ -3,8 +3,6 @@ import { defineConfig, externalizeDepsPlugin } from 'electron-vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import vueDevTools from 'vite-plugin-vue-devtools'
-import tailwindcss from 'tailwindcss'
-import autoprefixer from 'autoprefixer'
 
 import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
@@ -22,14 +20,6 @@ export default defineConfig({
       alias: {
         '@renderer': resolve('src/renderer/src')
       }
-    },
-    css: {
-      postcss: {
-        plugins: [
-          tailwindcss,
-          autoprefixer,
-        ],
-      },
     },
     plugins: [
       vue(),
